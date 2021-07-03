9138ecb8b09d30eab529dea518422ed7c64c60cb
Rohan Kacheria <rohankacheriam@gmail.com>
2021-07-03 11:29:37 +0530

    Email Spam Classifier

--- code.py
+++ code.py
@@ -9,30 +9,35 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 
 # reading the data
 df=pd.read_csv("./emails.csv")
-#print(df.head(10))
+print(df.head(10))
 
-# print(df.isnull().sum())
+# getting some basic info about the data
+print(df.isnull().sum())
 
-# print(df.describe())
+print(df.describe())
 
-# print(df.corr())
+print(df.corr())
 
 X=df.iloc[:, 1:3001]
 y=df.iloc[:, -1].values
 
-train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.25)
+# splitting the dataset into training and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
 
+# Using Naive Bayes model on the dataset
 mnb=MultinomialNB(alpha=1.9)
-mnb.fit(train_x, train_y)
-mnb_pred=mnb.predict(test_x)
-print("Accuracy Score for Naive Bayes: ", accuracy_score(mnb_pred, test_y))
+mnb.fit(X_train, y_train)
+mnb_pred=mnb.predict(X_test)
+print("Accuracy Score for Naive Bayes: ", accuracy_score(mnb_pred, y_test))
 
+# Using SVC model on the dataset
 svc=SVC(C=1.0, kernel="rbf", gamma="auto")
-svc.fit(train_x, train_y)
-svc_pred=svc.predict(test_x)
-print("Accuracy Score for SVC: ", accuracy_score(svc_pred, test_y))
+svc.fit(X_train, y_train)
+svc_pred=svc.predict(X_test)
+print("Accuracy Score for SVC: ", accuracy_score(svc_pred, y_test))
 
+# Using Random Forest Classification model on the dataset
 rfc=RandomForestClassifier(n_estimators=10, criterion="gini")
-rfc.fit(train_x, train_y)
-rfc_pred=rfc.predict(test_x)
-print("Accuracy Score for Random Forest Classification: ", accuracy_score(rfc_pred, test_y))
+rfc.fit(X_train, y_train)
+rfc_pred=rfc.predict(X_test)
+print("Accuracy Score for Random Forest Classification: ", accuracy_score(rfc_pred, y_test))
